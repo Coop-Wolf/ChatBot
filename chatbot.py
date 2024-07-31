c25351dcc725e919ab551badbba9724bb91847a1
Coop-Wolf <156962773+Coop-Wolf@users.noreply.github.com>
2024-07-31 20:21:14 +0000

    update

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,49 @@
-#pip install
-from chatterbot import ChatBot
+import re
+
+#from chatterbot import ChatBot
 #from chatterbot.trainers import ListTrainer
+#import time
+#time.clock = time.time
 
-import time
-time.clock = time.time
+#string to copy file
+play = ""
+Hamlet_train = []
 
-bob = ChatBot("SimpleBot")
-#trainer = ListTrainer(bob)
+# opening file and copying to string 
+# this was done because I wasn't sure
+# if I could use the file in the re.findall 
+# function directly
+with open("Ham.txt") as file:
+  for line in file:
+    for word in line.split():
+      play += word + " "
 
-#trainer.train(["What's your name?",
- #                 "My name is Alex",
-  #             "How old are you?",
-   #               "I am 2000 years old",
-   #            "How are you?"
-   #               "Fine, thank you",
-   #            "Are you going to take over the world",
-   #               "Yes",
-   #            "What is the meaning of life?
-   #               "To know, love and imitate Jesus"
-#               "Goodbye"
-#                  "Bye"])
+# using regex to store only the lines of Hamlet
+Hamlet = (re.findall("([A-Z][a-z\s][^\\.!?]*[\\.!?])", play))
+
+# going through list and printing each line
+for line in Hamlet:
+  print(line)
+  Hamlet_train.append(line)
+
+#Hamtrainer.train(Hamlet_train)
+
+'''
+HamletBot = ChatBot("SimpleBot")
+trainer = ListTrainer(HamletBot)
+
+trainer.train(["What's your name?",
+                  "My name is Hamlet",
+               "How old are you?",
+                  "I am 300 years old",
+               "How are you?"
+                  "Fine, thank you",
+               "Who killed you",
+                  "Laertes, he poisoned me",
+               "What is the meaning of life?
+                  "To know, love and imitate Jesus"
+               "Goodbye"
+                  "Bye"])
 
 exit_conditions = ("quit", "exit")
 
@@ -28,5 +52,6 @@ while True:
   if user_input in exit_conditions:
     break
   else:
-   responce = bob.get_response(user_input)
-   print(" :", responce)
+   response = HamletBot.get_response(user_input)
+   print("Hamlet:", responce)
+'''
